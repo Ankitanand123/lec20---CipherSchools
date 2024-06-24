c923dfd8ec9094c3c3087e9ad8a1a42b83f4f056
Ankitanand123 <117056428+Ankitanand123@users.noreply.github.com>
2024-06-24 13:00:13 +0530

    Create lec20.cpp

---
+++ lec20.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long int a;
+    cout<<"The size of long long int is = "<<sizeof(a)<<endl;
+    cout<<"The size of int is = "<<sizeof(int)<<endl;
+    cout<<"the adress of a is = "<<&a<<endl;
+    
+    
+
+    return 0;
+}
